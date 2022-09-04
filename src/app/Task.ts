4c29cf62dba11aae9188c082f64ed7980ca0084e
Leandro Nykolajczuk <108647874+Leonyko@users.noreply.github.com>
2022-09-04 10:53:50 -0300

    Task Complete

--- src/app/Task.ts
+++ src/app/Task.ts
@@ -2,5 +2,5 @@ export interface Task {
     id?:number; // cuando creamos podria n ovenir la id
     text: string;
     day: string;
-    remider: boolean;
+    reminder: boolean;
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http';
+import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASK } from '../mock-tasks';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
+
+const httpOptions = {
+  headers: new HttpHeaders ({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +21,22 @@ export class TaskService {
     private http:HttpClient
   ) { }
 
-  getTasks(): Observable<Task[]>{
-
+  getTasks(): Observable<Task[]>
+  {
     return this.http.get<Task[]>(this.apiUrl)
   }
+  deleteTask(task:Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
+  addTask(task:Task):Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
+
 }
